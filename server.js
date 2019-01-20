15d73606f4f8c81b8eee526df875ae94dc372239
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-01-20 21:04:09 +0200

    testing: listening on process.env.PORT || 3000

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send(JSON.stringify({ Hello: 'World'}));
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port !');
+});
